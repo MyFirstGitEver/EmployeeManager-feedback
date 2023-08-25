854021a4e66e16a81d927dda57f780e79ad36b0f
MyFirstGitEver <fordevok@gmail.com>
2023-08-25 09:47:28 +0700

    Initial Commit

---
+++ EmployeeManager/EmployeeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeManagerApp.swift
+//  EmployeeManager
+//
+//  Created by FVFH4069Q6L7 on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
